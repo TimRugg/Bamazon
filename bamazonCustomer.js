9f21a8fef2196b3c22c9aae2a1ff28852b0f42cf
timster2938 <ruggt001@icloud.com>
2018-02-11 12:14:04 -0500

    pseudo code customer

---
+++ bamazonCustomer.js
@@ -0,0 +1,13 @@
+const mySQL = Require("mySQL");
+
+// display all products with ID, name, and product
+
+// prompt user to select ID of product to buy
+
+// prompt user for how many?
+
+// check inventory against how many
+
+// if not enough - display message and start over
+
+// if enough - deduct from stock qty and show total of purchase
